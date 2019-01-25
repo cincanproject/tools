86c8a686e533acb8ccadc93fe07c63f765d996c0
Teemu Kontio <teemu.kontio@jamk.fi>
2019-01-25 14:14:07 +0200

    add dev image build for branches

--- detect_and_build.sh
+++ detect_and_build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+TAG=${TAG:-latest}
 for image in $(git diff --name-only HEAD~1|grep -Po "[^.]+(?=/)"|uniq)
 do
-   echo "Running: docker build -t cincan/$image:latest $image/."
-   docker build -t cincan/$image:latest $image/.
+   echo "Running: docker build -t cincan/$image:$TAG $image/."
+   docker build -t cincan/$image:$TAG $image/.
    
-   echo "Running: docker push cincan/$image:latest"
-   docker push cincan/$image:latest
+   echo "Running: docker push cincan/$image:$TAG"
+   docker push cincan/$image:$TAG
 done
