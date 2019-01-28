ba54750b24a8b24f0bd4e7cec0af3ef13fd30edb
erno <erno.kuusela@ficora.fi>
2019-01-28 16:01:45 +0200

    Fix ci script detection of changed images when many commits are pushed at once

--- detect_and_build.sh
+++ detect_and_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 TAG=${TAG:-latest}
-for image in $(git diff --name-only HEAD~1|grep -Po "[^.]+(?=/)"|uniq)
+for image in $(git diff --name-only $CI_COMMIT_BEFORE_SHA..HEAD|grep -Po "[^.]+(?=/)"|uniq)
 do
    echo "Running: docker build -t cincan/$image:$TAG $image/."
    docker build -t cincan/$image:$TAG $image/.
