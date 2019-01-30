df76297466be7728a9160f83ed36a2691b00aa9d
Teemu Kontio <teemu.kontio@jamk.fi>
2019-01-30 10:19:12 +0200

    re-add the fix from ba54750b24a8b24f0bd4e7cec0af3ef13fd30edb

--- detect_and_build.sh
+++ detect_and_build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 TAG=${TAG:-latest}
 echo -e "\e[4mFollowing has changed:\e[24m"
-echo -e "\e[36m $(git diff --name-only HEAD~1|grep -Po "^[^/]+(?=/)"|uniq)\e[39m"
+echo -e "\e[36m $(git diff --name-only $CI_COMMIT_BEFORE_SHA..HEAD|grep -Po "^[^/]+(?=/)"|uniq)\e[39m"
 
-for image in $(git diff --name-only HEAD~1|grep -Po "^[^/]+(?=/)"|uniq)
+for image in $(git diff --name-only $CI_COMMIT_BEFORE_SHA..HEAD|grep -Po "^[^/]+(?=/)"|uniq)
 do
    echo -e "\e[45mRunning: docker build -t cincan/$image:$TAG $image/.\e[49m"
    docker build -t cincan/$image:$TAG $image/.
