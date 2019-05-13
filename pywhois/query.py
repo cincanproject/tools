d6e032f6968e1b18c30b76d73f5b847f027d0378
Kimmo Linnavuo <kimmo.linnavuo@ficora.fi>
2019-05-13 16:40:37 +0300

    Add bulk whois option to pywhois tool

--- pywhois/query.py
+++ pywhois/query.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python3
 
-import whois
+from argparse import ArgumentParser, FileType
+import json
 import sys
+import whois
 
-try:
-    w = whois.whois(sys.argv[1])
-    print(w)
-except Exception as e:
-    print(e)
+def whois_query(ip):
+    try:
+        w = whois.whois(ip)
+        print(w)
+    except Exception as e:
+        json.dumps(e, indent=4)
+
+if __name__ == '__main__':
+    usage = "usage: %prog [options] ip"
+    parser = ArgumentParser(description="Query IP")
+
+    parser.add_argument("ip", nargs='?', help="ip to query")
+    parser.add_argument("-f", '--file', dest="file",
+                        help="file to load IPs from", type=FileType('r'))
+
+    args = parser.parse_args()
+
+    if args.file:
+        for ip in args.file:
+            whois_query(ip.strip())
+    else:
+        whois_query(args.ip)
+
