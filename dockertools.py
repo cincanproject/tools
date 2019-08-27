33fa3a0340b117f1323529f91ddbc75415bc4f6d
Rauli Kaksonen <rauli.kaksonen@gmail.com>
2019-08-27 15:12:19 +0300

    Allow to specify docker image and not file. Does not work yet

--- dockertools.py
+++ dockertools.py
@@ -8,14 +8,18 @@ from typing import List, Set, Dict, Tuple, Optional
 class ToolImage:
     """A tool wrapped to docker image"""
 
-    def __init__(self, file: str):
-        self.file = file
+    def __init__(self, file: Optional[str] = None, image: Optional[str] = None):
         self.logger = logging.getLogger(file)
         self.context = os.path.realpath(file)
         self.logger.info("context: %s", self.context)
         self.client = docker.from_env()
-        self.image, log = self.client.images.build(path=file)
-        self.do_log(log)
+        if file is not None:
+            self.image, log = self.client.images.build(path=file)
+            self.do_log(log)
+        elif image is not None:
+            self.image = self.client.images.get(image)
+        else:
+            raise Exception("No file nor image specified")
         # print("{} args={}".format(file, args))
 
     def run(self, args: List[str]):
@@ -39,11 +43,17 @@ def main():
     subparsers = m_parser.add_subparsers()
     run_parser = subparsers.add_parser('run')
     run_parser.add_argument('-p', '--path')
+    run_parser.add_argument('-i', '--image')
     run_parser.add_argument('-a', '--args', nargs='*')
     args = m_parser.parse_args()
     if args.logLevel:
         logging.basicConfig(level=getattr(logging, args.logLevel))
-    tool = ToolImage(args.path)
+    if args.image is not None:
+        tool = ToolImage(image=args.image)
+    elif args.path is not None:
+        tool = ToolImage(file=args.path)
+    else:
+        tool = ToolImage() # should raise exception
     print(tool.run(args.args))
 
 
