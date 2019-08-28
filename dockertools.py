8e554672e721faaa1adc5944e023f1e5bfe62948
Rauli Kaksonen <rauli.kaksonen@gmail.com>
2019-08-28 12:33:57 +0300

    Upload a single input file to docker container

--- dockertools.py
+++ dockertools.py
@@ -1,6 +1,8 @@
 import argparse
 import docker
 import logging
+import tarfile
+import io
 import os
 from typing import List, Set, Dict, Tuple, Optional
 
@@ -11,6 +13,7 @@ class ToolImage:
     def __init__(self, file: Optional[str] = None, image: Optional[str] = None):
         self.logger = logging.getLogger(file)
         self.context = os.path.realpath(file)
+        self.temp_dir = os.path.realpath('cincan-temp')
         self.logger.info("context: %s", self.context)
         self.client = docker.from_env()
         if file is not None:
@@ -20,12 +23,26 @@ class ToolImage:
             self.image = self.client.images.get(image)
         else:
             raise Exception("No file nor image specified")
-        self.container = None
-        # print("{} args={}".format(file, args))
+        self.tarball = None
+
+    def copy_files_to_temp(self, files: List[str]) -> None:
+        file_out = io.BytesIO()
+        tar = tarfile.open(mode="w", fileobj=file_out)
+        for p in files:
+            self.logger.debug("in-file %s", p)
+            tar.add(p)
+        tar.close()
+        self.tarball = file_out.getvalue()
+        self.logger.debug("Tarball to upload, size %d", len(self.tarball))
+        # with open("files_to_upload.tar", "wb") as f:
+        #     f.write(self.tarball)
 
     def run(self, args: List[str]):
         samples_dir = self.context + '/samples'
         container = self.client.containers.create(self.image, volumes={samples_dir: {'bind': "/samples"}}, command=args)
+        if self.tarball is not None:
+            # container.exec_run(cmd='mkdir -p /tmp/cincan-work')
+            container.put_archive(path='/tmp', data=self.tarball)
         container.start()
         container.wait()
         return container.attach(logs=True)
@@ -48,6 +65,8 @@ def main():
     run_parser = subparsers.add_parser('run')
     run_parser.add_argument('-p', '--path')
     run_parser.add_argument('-i', '--image')
+    run_parser.add_argument('-s', '--in-string')
+    run_parser.add_argument('-f', '--in-file')
     run_parser.add_argument('-a', '--args', nargs='*')
     args = m_parser.parse_args()
     if args.logLevel:
@@ -58,6 +77,8 @@ def main():
         tool = ToolImage(file=args.path)
     else:
         tool = ToolImage() # should raise exception
+    if args.in_file is not None:
+        tool.copy_files_to_temp([args.in_file])
     print(tool.run(args.args))
 
 
