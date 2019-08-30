7037db573e66c27ad4c33592508c7023c6da649a
Rauli Kaksonen <rauli.kaksonen@gmail.com>
2019-08-30 09:26:46 +0300

    Specify path parameter by name

--- tshark/test_image.py
+++ tshark/test_image.py
@@ -2,6 +2,6 @@ from .. import dockertools
 
 
 def test_image():
-    tool = dockertools.ToolImage("tshark")
+    tool = dockertools.ToolImage(path="tshark")
     out = tool.run_get_string(["-r", "^tshark/samples/ping_localhost.pcap", "-c", "2", "-T", "json"])
     assert out == 'FIXME\n'
--- xmldump/test_image.py
+++ xmldump/test_image.py
@@ -2,6 +2,6 @@ from .. import dockertools
 
 
 def test_image():
-    tool = dockertools.ToolImage("xmldump")
+    tool = dockertools.ToolImage(path="xmldump")
     out = tool.run_get_string(["text", "^xmldump/samples/simple.xml"])
     assert out == 'Hello World\n'
