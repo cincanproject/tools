2737d730c915b807e5e182903ae7509ad632ff8b
Rauli Kaksonen <rauli.kaksonen@gmail.com>
2019-08-30 15:19:15 +0300

    pdf-tools unit test. Does not pass

---
+++ pdf-tools/test_pdf_tools.py
@@ -0,0 +1,19 @@
+import dockertools
+import re
+
+pattern = re.compile("^\\s*(\\S+)\\s+(\\S.*)$")
+
+
+def test_image():
+    tool = dockertools.ToolImage(path="pdf-tools")
+    out = str(tool.run(["python", "base64dump.py", tool.file_to_copy("samples/text_txt.pdf")]))
+    values = {}
+    for line in out.splitlines():
+        m = re.match(pattern, line)
+        if m is not None:
+            print("X: {}({})".format(m.group(1), m.group(2)))
+            values[m.group(1)] = m.group(2)
+    assert values['obj'] == '13'
+    assert values['endobj'] == '13'
+    assert values['stream'] == '2'
+    assert values['xref'] == '1'
