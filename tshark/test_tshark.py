6d5be5b655ca5067e8ff022ecbcc80939d3f9d21
Rauli Kaksonen <rauli.kaksonen@gmail.com>
2019-09-05 12:35:05 +0300

    Unit test hints

--- tshark/test_tshark.py
+++ tshark/test_tshark.py
@@ -9,3 +9,9 @@ def test_image():
     assert js[0]['_index'] == 'packets-2019-09-05'
     assert js[0]['_source']['layers']['ip']['ip.src'] == '127.0.0.1'
     assert len(js) == 2
+
+
+def test_hints():
+    tool = dockertools.ToolImage(path="tshark")
+    hints = tool.list_command_line()
+    assert "|".join(hints) == "-r ^<file> -Tjson|-r ^<file> -Tpdml"
