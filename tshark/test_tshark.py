358da92bde72d904abe9c591aeee55b302c07cb6
Rauli Kaksonen <rauli.kaksonen@gmail.com>
2019-09-05 10:34:25 +0300

    Proper assertions for tshark test

--- tshark/test_tshark.py
+++ tshark/test_tshark.py
@@ -1,7 +1,11 @@
 import dockertools
+import json
 
 
 def test_image():
     tool = dockertools.ToolImage(path="tshark")
     out = tool.run_get_string(["-r", tool.file_to_copy("samples/ping_localhost.pcap"), "-c", "2", "-T", "json"])
-    assert out == 'FIXME\n'
+    js = json.loads(out)
+    assert js[0]['_index'] == 'packets-2019-09-05'
+    assert js[0]['_source']['layers']['ip']['ip.src'] == '127.0.0.1'
+    assert len(js) == 2
