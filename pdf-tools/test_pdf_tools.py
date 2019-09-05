a925ccb362219a68fc7216b0bb357a9eeea9bda9
Rauli Kaksonen <rauli.kaksonen@gmail.com>
2019-09-05 10:52:15 +0300

    Fixed unit test for pdf-tool

--- pdf-tools/test_pdf_tools.py
+++ pdf-tools/test_pdf_tools.py
@@ -1,19 +1,20 @@
 import dockertools
 import re
 
-pattern = re.compile("^\\s*(\\S+)\\s+(\\S.*)$")
+pattern = re.compile("^\\s*(\\S+)\\s+(\\S+)\\s+(\\S+)\\s+(\\S+)\\s+(\\S+)\\s*$")
 
 
 def test_image():
     tool = dockertools.ToolImage(path="pdf-tools")
-    out = str(tool.run(["python", "base64dump.py", tool.file_to_copy("samples/text_txt.pdf")]))
-    values = {}
+    out = tool.run(["python", "base64dump.py", tool.file_to_copy("samples/text_txt.pdf")]).decode('utf8')
+    values = []
     for line in out.splitlines():
         m = re.match(pattern, line)
         if m is not None:
-            print("X: {}({})".format(m.group(1), m.group(2)))
-            values[m.group(1)] = m.group(2)
-    assert values['obj'] == '13'
-    assert values['endobj'] == '13'
-    assert values['stream'] == '2'
-    assert values['xref'] == '1'
+            # print("{} {} {}".format(m.group(1), m.group(3), m.group(5)))
+            values.append((m.group(1), m.group(3), m.group(5)))
+    assert values[1][0] == '1:'
+    assert values[1][1] == 'R/Filter'
+    assert values[1][2] == '8e306826ca2c662dc71e92c6bf8eaef5'
+    assert values[2][0] == '2:'
+    assert values[3][0] == '3:'
