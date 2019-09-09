6d0e7043d277bddf96ba0568df6ae0564b4b44a9
Niklas Saari <niklas.saari@student.oulu.fi>
2019-09-09 14:09:10 +0300

    Added check for dockerfile

--- detect_and_build.sh
+++ detect_and_build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 TAG=${TAG:-latest}
 echo -e "\e[4mFollowing has changed:\e[24m"
-echo -e "\e[36m $(git diff --name-only $CI_COMMIT_BEFORE_SHA..HEAD|grep -Po "^[^/]+(?=/)"|uniq)\e[39m"
+echo -e "\e[36m $(git diff --name-only "$CI_COMMIT_BEFORE_SHA"..HEAD|grep -Po "^[^/]+(?=/)"|uniq)\e[39m"
 docker info
 
-for image in $(git diff --name-only $CI_COMMIT_BEFORE_SHA..HEAD|grep -Po "^[^/]+(?=/)"|uniq)
+for image in $(git diff --name-only "$CI_COMMIT_BEFORE_SHA"..HEAD|grep -Po "^[^/]+(?=/)"|uniq)
 do
+   if test -f "$image/Dockerfile"; then
    echo -e "\e[45mRunning: docker build -t cincan/$image:$TAG $image/.\e[49m"
-   docker build -t cincan/$image:$TAG $image/.
+   docker build -t cincan/"$image":"$TAG" "$image"/.
    
    echo -e "\e[45mRunning: docker push cincan/$image:$TAG\e[49m"
-   docker push cincan/$image:$TAG
+   docker push cincan/"$image":"$TAG"
+   fi
 done
