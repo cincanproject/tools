837a49a126d66b7464f976ce37f8dc0ccc986615
Rauli Kaksonen <rauli.kaksonen@gmail.com>
2019-09-11 10:41:48 +0300

    Initial tests for all tool images. Many fail

---
+++ pdf2john/test_pdf2john.py
@@ -0,0 +1,7 @@
+import dockertools
+
+
+def test_help():
+    tool = dockertools.tool_with_file(__file__)
+    out = tool.run_get_string([])
+    assert out.startswith("Syntax:  pdf2john.pl <.pdf file(s)>")
---
+++ peepdf/test_peepdf.py
@@ -0,0 +1,8 @@
+import dockertools
+
+
+def test_help():
+    assert False
+    # tool = dockertools.tool_with_file(__file__)
+    # out = tool.run_get_string([])
+    # assert out.startswith("FIXME")
