304dbff5e86a7488401b67954d053b5bf87896d5
Rauli Kaksonen <rauli.kaksonen@gmail.com>
2019-09-18 14:30:17 +0300

    Moved command line creation to ToolCommands class

--- metatool/commands.py
+++ metatool/commands.py
@@ -6,6 +6,35 @@ class ToolCommands:
     def __init__(self, json: List[Dict[str,Any]]):
         self.json = json
 
+    def command_line(self, in_file: str, args: List[str] = None,
+                     in_type: Optional[str] = None, out_type: Optional[str] = None) -> List[str]:
+        """Create actual command line"""
+        all_commands = self.json
+        match_commands = []
+        for c in self.json:
+            if '<file>' not in c['command']:
+                continue
+            if in_type is not None and in_type not in c.get('input'):
+                continue
+            if out_type is not None and out_type not in c.get('output'):
+                continue
+            match_commands.append(c)
+        if len(match_commands) != 1:
+            raise Exception("Single command should match given input/output filter, now:\n{}".format(
+                "\n".join(map(lambda x: "{}{}".format(" -i {}".format(x['input']) if 'input' in x else "",
+                                                      " -o {}".format(x['output']) if 'output' in x else ""),
+                              match_commands))))
+        command = match_commands[0]['command']
+        true_args = []
+        for arg in command:
+            if arg == "<file>":
+                true_args.append("^{}".format(in_file))
+            else:
+                true_args.append(arg)
+        for arg in args if args is not None else []:
+            true_args.append(arg)
+        return true_args
+
     def command_hints(self) -> List[str]:
         lines = []
         for c in self.json:
--- metatool/dockertools.py
+++ metatool/dockertools.py
@@ -194,32 +194,9 @@ class ToolImage:
     def do_run(self, in_file: str, args: List[str] = None,
                in_type: Optional[str] = None, out_type: Optional[str] = None) -> bytes:
         """Do -sub command to run the native tool"""
-        all_commands = self.get_commands()
-        match_commands = []
-        for c in all_commands.json:
-            if '<file>' not in c['command']:
-                continue
-            if in_type is not None and in_type not in c.get('input'):
-                continue
-            if out_type is not None and out_type not in c.get('output'):
-                continue
-            match_commands.append(c)
-        if len(match_commands) != 1:
-            raise Exception("Single command should match given input/output filter, now:\n{}".format(
-                "\n".join(map(lambda x: "{}{}".format(" -i {}".format(x['input']) if 'input' in x else "",
-                                                      " -o {}".format(x['output']) if 'output' in x else ""),
-                              match_commands))))
-        command = match_commands[0]['command']
-        true_args = []
-        for arg in command:
-            if arg == "<file>":
-                true_args.append("^{}".format(in_file))
-            else:
-                true_args.append(arg)
-        for arg in args if args is not None else []:
-            true_args.append(arg)
-        self.logger.debug(" ".join(true_args))
-        return self.run(true_args)
+        cmd_line = self.get_commands().command_line(in_file, args, in_type, out_type)
+        self.logger.debug(" ".join(cmd_line))
+        return self.run(cmd_line)
 
     def do_get_string(self, in_file: str, args: List[str] = None,
                       in_type: Optional[str] = None, out_type: Optional[str] = None) -> str:
