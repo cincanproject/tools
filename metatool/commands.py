5ad9f91b9d7bb1f9e4ae58c6b373064ae978d44a
Rauli Kaksonen <rauli.kaksonen@gmail.com>
2019-09-19 09:13:23 +0300

    Use pattern to match files and directories from commands

--- metatool/commands.py
+++ metatool/commands.py
@@ -1,3 +1,5 @@
+import re
+
 from typing import List, Set, Dict, Tuple, Optional, Any
 
 
@@ -6,6 +8,7 @@ class ToolCommands:
     def __init__(self, json: Dict[str,Any]):
         self.commands_json = json['commands']
         self.outputs_json = json.get('output-options', [])
+        self.file_pattern = re.compile("(<[^>]*(file|dir)>)")
 
     def is_stdout_output_option(self) -> bool:
         for t in self.outputs_json:
@@ -26,7 +29,7 @@ class ToolCommands:
         all_commands = self.commands_json
         match_commands = []
         for c in self.commands_json:
-            if '<file>' not in c['command']:
+            if not any(map(lambda s: self.file_pattern.match(s), c['command'])):
                 continue
             if in_type is not None and in_type not in c.get('input'):
                 continue
@@ -41,10 +44,7 @@ class ToolCommands:
         command = match_commands[0]['command']
         true_args = []
         for arg in command:
-            if arg == "<file>":
-                true_args.append("^{}".format(in_file))
-            else:
-                true_args.append(arg)
+            true_args.append(self.file_pattern.sub("^" + in_file, arg))
         for arg in args if args is not None else []:
             true_args.append(arg)
         return true_args
@@ -55,9 +55,9 @@ class ToolCommands:
         output_opt = self.get_output_to_file_option()
         for c in self.commands_json:
             cmds = c['command']
-            str = " ".join(cmds).replace("<file>", "^<file>")
+            str = self.file_pattern.sub("^\\1", " ".join(cmds))
             if output_opt:
-                opt_str = " ".join(output_opt).replace("<file>", "^^<file>")
+                opt_str = self.file_pattern.sub("^^\\1", " ".join(output_opt))
                 if stdout_opt:
                     opt_str = "[" + opt_str + "]"
                 str = opt_str + " " + str
