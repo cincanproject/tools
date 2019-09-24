75e62120fa8edb2a91b57e7deb0c0cfd13cc4f53
Rauli Kaksonen <rauli.kaksonen@gmail.com>
2019-09-24 15:13:57 +0300

    Test input file, output tarfile

--- xmldump/test_xmldump.py
+++ xmldump/test_xmldump.py
@@ -31,6 +31,19 @@ def test_do_run():
     assert out == b'Hello World\n'
 
 
+def test_do_run_read_file():
+    tool = dockertools.tool_with_file(__file__)
+    tool.output_tar = "/tmp" + __file__ + ".tar"
+    tool.do_run(in_file=tool.file_to_copy_from_context("samples/simple.xml", prefix=False))
+    with tarfile.open(tool.output_tar) as tar:
+       meta = json.load(tar.extractfile(tool.metadata_file))
+       out = tar.extractfile("output").read()
+    os.unlink(tool.output_tar)
+    assert meta['files'][0]['name'] == "output"
+    assert meta['files'][0]['type'] == "text/plain"
+    assert out == b'Hello World\n'
+
+
 def test_do_run_get_string():
     tool = dockertools.tool_with_file(__file__)
     out = tool.do_get_string(in_file=tool.file_to_copy_from_context("samples/simple.xml", prefix=False))
