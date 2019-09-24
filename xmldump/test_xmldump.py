b9bf3a2e4319b996fb4b0da62f7059f8e21bd92c
Rauli Kaksonen <rauli.kaksonen@gmail.com>
2019-09-24 15:10:22 +0300

    Check input/output tar using xmldump

--- xmldump/test_xmldump.py
+++ xmldump/test_xmldump.py
@@ -1,3 +1,7 @@
+import json
+import os
+import tarfile
+
 from metatool import dockertools
 
 
@@ -14,6 +18,20 @@ def test_xml_to_text():
 
 
 def test_do_run():
+    tool = dockertools.tool_with_file(__file__)
+    tool.upload_tar = tool.file_to_copy_from_context("samples/simple.tar", prefix=False)
+    tool.output_tar = "/tmp" + __file__ + ".tar"
+    tool.do_run()
+    with tarfile.open(tool.output_tar) as tar:
+       meta = json.load(tar.extractfile(tool.metadata_file))
+       out = tar.extractfile("output").read()
+    os.unlink(tool.output_tar)
+    assert meta['files'][0]['name'] == "output"
+    assert meta['files'][0]['type'] == "text/plain"
+    assert out == b'Hello World\n'
+
+
+def test_do_run_get_string():
     tool = dockertools.tool_with_file(__file__)
     out = tool.do_get_string(in_file=tool.file_to_copy_from_context("samples/simple.xml", prefix=False))
     assert out == 'Hello World\n'
