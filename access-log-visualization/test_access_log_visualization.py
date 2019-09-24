b981deec075940fb4d215b854bde1a730d11040f
Rauli Kaksonen <rauli.kaksonen@gmail.com>
2019-09-24 11:44:22 +0300

    Fixed unit test, as stdout comes now first as output

--- access-log-visualization/test_access_log_visualization.py
+++ access-log-visualization/test_access_log_visualization.py
@@ -4,4 +4,4 @@ from metatool import dockertools
 def test_help():
     tool = dockertools.tool_with_file(__file__)
     out = tool.run_get_string([])
-    assert out.startswith("[NbConvertApp] WARNING | pattern '/samples/access-log.ipynb' matched no files")
+    assert out.startswith("This application is used to convert notebook files (*.ipynb) to various other")
