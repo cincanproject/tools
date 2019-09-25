5bce2363827fb81608229f04314903de59121bca
Rauli Kaksonen <rauli.kaksonen@gmail.com>
2019-09-25 10:25:01 +0300

    Indent fixes

--- xmldump/test_xmldump.py
+++ xmldump/test_xmldump.py
@@ -23,8 +23,8 @@ def test_do_run():
     tool.output_tar = "/tmp" + __file__ + ".tar"
     tool.do_run()
     with tarfile.open(tool.output_tar) as tar:
-       meta = json.load(tar.extractfile(tool.metadata_file))
-       out = tar.extractfile("output").read()
+        meta = json.load(tar.extractfile(tool.metadata_file))
+        out = tar.extractfile("output").read()
     os.unlink(tool.output_tar)
     assert meta['files'][0]['name'] == "output"
     assert meta['files'][0]['type'] == "text/plain"
@@ -36,8 +36,8 @@ def test_do_run_read_file():
     tool.output_tar = "/tmp" + __file__ + ".tar"
     tool.do_run(in_file=tool.file_to_copy_from_context("samples/simple.xml", prefix=False))
     with tarfile.open(tool.output_tar) as tar:
-       meta = json.load(tar.extractfile(tool.metadata_file))
-       out = tar.extractfile("output").read()
+        meta = json.load(tar.extractfile(tool.metadata_file))
+        out = tar.extractfile("output").read()
     os.unlink(tool.output_tar)
     assert meta['files'][0]['name'] == "output"
     assert meta['files'][0]['type'] == "text/plain"
