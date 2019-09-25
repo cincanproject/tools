8be6088bba30648fb38340d7619d3148f7e99250
Rauli Kaksonen <rauli.kaksonen@gmail.com>
2019-09-25 22:13:27 +0300

    Check if output is stdout, and do not write other data to mess it up

--- metatool/dockertools.py
+++ metatool/dockertools.py
@@ -473,7 +473,7 @@ def main():
             elif not read_file:
                 raise Exception('Must specify either --in, --in-file, or --in-str')
             ret = tool.do_run(in_file=read_file, args=all_args, in_type=args.in_type, out_type=args.out_type)
-            if tool.get_commands().get_output_to_file_option():
+            if tool.output_tar != '-' and tool.get_commands().get_output_to_file_option():
                 # content is handled through output file, dump stdout visible as it should not contain the data
                 sys.stdout.buffer.write(ret[0])
             sys.stderr.buffer.write(ret[1])
