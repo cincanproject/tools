2cbbbc617cc4d99ad04004d69c7f316957a02728
Rauli Kaksonen <rauli.kaksonen@gmail.com>
2019-10-09 10:55:17 +0300

    Removed mistakenly committed preserve_image=True arguments

--- test2tool/test_tool.py
+++ test2tool/test_tool.py
@@ -5,13 +5,13 @@ from metatool import dockertools
 
 def test_echo():
     tool = dockertools.tool_with_file(__file__)
-    out = tool.run_get_string(["echo HelloWorld"], preserve_image=True)
+    out = tool.run_get_string(["echo HelloWorld"])
     assert out == "HelloWorld\n"
 
 
 def test_mkdir_output():
     tool = dockertools.tool_with_file(__file__)
-    out = tool.run_get_string(["cd ^^test_out_dir/; echo HelloWorldEnd > a.txt"], preserve_image=True)
+    out = tool.run_get_string(["cd ^^test_out_dir/; echo HelloWorldEnd > a.txt"])
     with open("test_out_dir/a.txt", "r") as f:
         out_data = f.read()
     os.unlink("test_out_dir/a.txt")
