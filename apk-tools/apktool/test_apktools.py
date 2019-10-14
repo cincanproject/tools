2cdfbaf1eff5a3acd96700cad54180db74075d10
Karoliina <karoliina.kemppainen@traficom.fi>
2019-10-14 11:59:51 +0200

    Working docker files for apktool and dex2jar. Changed folder structure.

---
+++ apk-tools/apktool/test_apktools.py
@@ -0,0 +1,9 @@
+from metatool import dockertools
+import json
+import pytest
+
+
+def test_help():
+    tool = dockertools.tool_with_file(__file__)
+    out = tool.run_get_string([])
+    assert out.startswith("Usage:")
