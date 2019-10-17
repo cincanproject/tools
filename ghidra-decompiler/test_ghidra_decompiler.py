7a6c3924205999b2764009186e1c2c491f6e6e64
Niklas Saari <niklas.saari@student.oulu.fi>
2019-10-17 21:02:26 +0300

    Ghidra test updated

--- ghidra-decompiler/test_ghidra_decompiler.py
+++ ghidra-decompiler/test_ghidra_decompiler.py
@@ -4,4 +4,4 @@ from metatool import dockertools
 def test_no_args_no_output():
     tool = dockertools.tool_with_file(__file__)
     out = tool.run_get_string([])
-    assert out == ""
+    assert out.strip().startswith('Exception in thread "main" ghidra.util.exception.InvalidInputException: -import requires an argument')
