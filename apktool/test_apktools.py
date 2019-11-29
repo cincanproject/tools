668705d34dcab2ac30773e0653f28d635ce37453
Karoliina Kemppainen <karoliina.kemppainen@traficom.fi>
2019-11-29 10:45:35 +0200

    modifications to apktool

--- apktool/test_apktools.py
+++ apktool/test_apktools.py
@@ -1,4 +1,7 @@
 from metatool import dockertools
+from os import path
+import shutil
+
 
 def test_help():
     tool = dockertools.tool_with_file(__file__)
@@ -7,5 +10,6 @@ def test_help():
 
 def test_apk_to_jar():
     tool = dockertools.tool_with_file(__file__)
-    out = tool.run_get_string(["d", tool.file_to_copy_from_context("samples/selendroid-test-app.apk", "o" "selendroid-test-app-test")])
+    out = tool.run_get_string(["d", tool.file_to_copy_from_context("samples/selendroid-test-app.apk")])
+    print(out)
     assert out.startswith("I: Using Apktool 2.4.0 on selendroid-test-app.apk")
