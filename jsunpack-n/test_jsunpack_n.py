af18056eaee178640319eabf76892322dc024987
Niklas Saari <niklas.saari@student.oulu.fi>
2019-12-05 23:15:44 +0200

    Fix jsunpack-n decprecated dependancy, update tests to support latest version, move sample file

--- jsunpack-n/test_jsunpack_n.py
+++ jsunpack-n/test_jsunpack_n.py
@@ -1,12 +1,16 @@
 from metatool import dockertools
+import pytest
+
+SAMPLE_FILE="_samples/pdf/shellcode_test.pdf"
 
 def test_help():
     tool = dockertools.tool_with_file(__file__)
     out = tool.run_get_string([])
     assert out.startswith("Usage: \n\t./jsunpackn.py [fileName")
 
+@pytest.mark.slow
 def test_with_pdf():
     tool = dockertools.tool_with_file(__file__)
-    out = tool.run_get_string([tool.file_to_copy_from_context("samples/testfile.pdf"), "-V"])
+    out = tool.run_get_string([SAMPLE_FILE, "-V"])
     assert "decoding_530752faa103f6929a7f067eee4088517970e6a9" in out.splitlines()[3]
 
