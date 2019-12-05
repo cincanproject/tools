f1948ef4a455cb3b82f942e81baad7c3b3c9706f
Niklas Saari <niklas.saari@student.oulu.fi>
2019-12-05 20:58:36 +0200

    Move oledump sample, make tests work with latest setup

--- oledump/test_oledump.py
+++ oledump/test_oledump.py
@@ -1,5 +1,7 @@
 from metatool import dockertools
 
+SAMPLE_FILE="_samples/msoffice/testfile.docm"
+
 def test_help():
     tool = dockertools.tool_with_file(__file__)
     out = tool.run_get_string([])
@@ -7,6 +9,6 @@ def test_help():
 
 def test_with_document():
     tool = dockertools.tool_with_file(__file__)
-    out = tool.run_get_string([tool.file_to_copy_from_context("samples/testfile.docm"), "-S", "-s", "3"])
+    out = tool.run_get_string([SAMPLE_FILE, "-S", "-s", "3"])
     assert "CinCan" in out.splitlines()[1]
 
