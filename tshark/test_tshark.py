910bbc4a73630b2e83d3e5947cc20d2e75bf5f43
Niklas Saari <niklas.saari@student.oulu.fi>
2019-12-05 20:45:16 +0200

    Make tshark to work with latest test setup, move sample file

--- tshark/test_tshark.py
+++ tshark/test_tshark.py
@@ -2,16 +2,16 @@ from metatool import dockertools
 import json
 import pytest
 
+SAMPLE_FILE="_samples/pcap/ping_localhost.pcap"
 
 def test_help():
     tool = dockertools.tool_with_file(__file__)
     out = tool.run_get_string([])
     assert out.startswith('TShark (Wireshark)')
 
-
 def test_pcap_to_json():
     tool = dockertools.tool_with_file(__file__)
-    out = tool.run_get_string(["-r", tool.file_to_copy_from_context("samples/ping_localhost.pcap"), "-c", "2", "-T", "json"])
+    out = tool.run_get_string(["-r", SAMPLE_FILE, "-c", "2", "-T", "json"])
     js = json.loads(out)
     assert js[0]['_source']['layers']['ip']['ip.src'] == '127.0.0.1'
     assert len(js) == 2
@@ -21,11 +21,11 @@ def test_do_run():
     tool = dockertools.tool_with_file(__file__)
 
     out = tool.run_get_string(
-        ["-r", tool.file_to_copy_from_context("samples//ping_localhost.pcap"), "-c", "2", "-Tjson"])
+        ["-r", SAMPLE_FILE, "-c", "2", "-Tjson"])
     assert out.startswith("[")
 
     out = tool.run_get_string(
-        ["-r", tool.file_to_copy_from_context("samples//ping_localhost.pcap"), "-c", "2", "-Tpdml"])
+        ["-r", SAMPLE_FILE, "-c", "2", "-Tpdml"])
     assert out.startswith("<?xml")
 
     res = tool.run(["-r", "nosuchfile.pcap", "-c", "2", "-Tpdml"])
