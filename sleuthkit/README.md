cba7831cd4f17378d4fcab8104b878a8e075a635
Kimmo Linnavuo <kimmo.linnavuo@ficora.fi>
2019-12-17 15:48:14 +0200

    Sleuthkit container and tests

--- sleuthkit/README.md
+++ sleuthkit/README.md
@@ -1,14 +1,55 @@
 # "Open Source Digital Forensics"
 
+## Input
+
+```
+raw, ewf, vmdk, vhd
+```
+
+## Output
+
+```
+multiple outputs
+```
+
+
 ## Supported tags and respective `Dockerfile` links
 
 * `latest` 
 ([*sleuthkit/Dockerfile*](https://gitlab.com/CinCan/dockerfiles/blob/master/sleuthkit/Dockerfile))
 
+
 ## Usage
+
+***1. Clone the repository***
+
 ```
-docker run -it -v $(pwd)/samples:/samples cincan/sleuthkit [tool from sleuthkit]
-``` 
+git clone https://gitlab.com/CinCan/tools.git
+cd tools/sleuthkit
+
+***2. Build OR pull the docker image***
+
+```
+docker build . -t cincan/sleuthkit
+docker pull cincan/sleuthkit
+```
+
+***3. Run the docker container***  
+
+Example 1. List file system information with `fsstat`  
+
+`$ docker run --rm -v $(pwd):/input cincan/sleuthkit fsstat /input/testdisk.raw`  
+
+Example 2. List files and directories with `fls`  
+
+`$ docker run --rm -v $(pwd):/input cincan/sleuthkit fls /input/testdisk.raw`  
+
+Example 3. Dump all unallocated units of a file system with `blkls` using the CinCan tool:    
+
+`$ cincan run cincan/sleuthkit blkls /input/testdisk.raw`  
+
+
 ## Project homepage
 
-https://www.sleuthkit.org/
+https://www.sleuthkit.org/sleuthkit/
+
--- sleuthkit/test_sleuthkit.py
+++ sleuthkit/test_sleuthkit.py
@@ -1,9 +1,37 @@
 from metatool import dockertools
 import pytest
 
-@pytest.mark.dev
+SAMPLE_FILE="_samples/disks/7-ntfs-undel.dd"
 
-def test_help():
+def test_tools_help(tool):
+    out = tool.run_get_string(["help"])
+    assert "fsstat" in out
+    assert "fls" in out
+
+def test_fsstat(tool):
+    out = tool.run_get_string(["fsstat", SAMPLE_FILE])
+    assert "285C576D5C5734B2" in out
+    assert "NTFS_DEL" in out
+    assert "First Cluster of MFT: 2005" in out
+    assert "Sector Size: 512" in out
+
+def test_blkstat(tool):
+    out = tool.run_get_string(["blkstat", SAMPLE_FILE, "3"])
+    assert "Allocated" in out
+
+def test_ils(tool):
+    out = tool.run_get_string(["ils", SAMPLE_FILE])
+    assert "29|f|0|0|1078084840|1078084840|1078084840|1078084817|777|1|1584" in out
+ 
+def test_fls_deleted_entries(tool):
     tool = dockertools.tool_with_file(__file__)
-    out = tool.run_get_string([])
-    assert out.startswith("Missing image name")
+    out = tool.run_get_string(["fls", "-d", SAMPLE_FILE])
+    assert "frag1.dat" in out
+    assert "mult1.dat" in out
+    assert "dir1" in out
+
+@pytest.fixture(scope='function')
+def tool(request):
+    tool = dockertools.tool_with_file(__file__)
+    yield tool
+
