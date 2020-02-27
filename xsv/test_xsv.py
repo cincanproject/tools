b07439591880fdf6ad3cc0df33cd4b9b7f802f94
Erno Kuusela <erno.kuusela@ficora.fi>
2020-02-27 16:42:33 +0200

    add test for xsv

---
+++ xsv/test_xsv.py
@@ -0,0 +1,14 @@
+from metatool import dockertools
+import pytest
+
+
+def test_count(tool):
+    out = tool.run_get_string(["frequency", "_samples/test.csv"])
+    print(out)
+    assert "foo,42,1" in out
+
+@pytest.fixture(scope='function')
+def tool(request):
+    tool = dockertools.tool_with_file(__file__)
+    yield tool
+
