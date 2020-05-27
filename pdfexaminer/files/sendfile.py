f18d0c36301266d12312173300307314d37e01a9
Vesa Vertainen <vesa.vertainen@jamk.fi>
2020-05-27 16:25:58 +0300

    some fixes to code

--- pdfexaminer/files/sendfile.py
+++ pdfexaminer/files/sendfile.py
@@ -1,42 +1,60 @@
 import time
-from tqdm import tqdm
 import threading
 import sys
 import requests
 import argparse
 
-# Progress bar
-def progress_bar(stop_thread):
-    t2 = threading.currentThread()
-    with tqdm(total=100, file=sys.stdout) as pbar:
+class PdfexaminerSendfile:
+    def __init__(self):
+        self.__report = ""
+
+    def get_report(self):
+        self.__report = self.__report.text.strip()
+        return self.__report
+
+    def set_report(self, report):
+        self.__report = report
+
+
+    # Progress bar
+    @staticmethod
+    def progress_bar(stop_thread):
+        t2 = threading.currentThread()
+        x = 0
+        loading = [" Loadin`"," Loadiñ "," Loadìn "," Loaðin "," Loâdin "," Lõadin", " £oadin ", "~Loadin", " Loadin~"]
         while getattr(t2, "run", True):
-            time.sleep(0.5)
-            pbar.update(10)
+            time.sleep(0.05)
+            if x < len(loading)-1:
+                x = x + 1
+            else:
+                x = 0
+            print (loading[x], end="\r")
 
 
-# Parse passed arguments
-def parse_arguments():
-    parser = argparse.ArgumentParser(description='PDFExaminer')
-    parser.add_argument('-i', '--input', required=True, help="")
-    parser.add_argument('-f', '--format', required=False, help="json, xml, ioc, php, severity, rating, is_malware, text")
-    return parser.parse_args()
+    # Parse passed arguments
+    @staticmethod
+    def parse_arguments():
+        parser = argparse.ArgumentParser(description='PDFExaminer')
+        parser.add_argument('-i', '--input', required=True, help="")
+        parser.add_argument('-f', '--format', required=False, help="json, xml, ioc, php, severity, rating, is_malware, text")
+        return parser.parse_args()
 
 
-# Send file
-def send_file(input, datatype):
-    t = threading.currentThread()
-    url = 'https://www.pdfexaminer.com/pdfapi.php'
-    print("Sending " + input)
-    files = {'sample[]': open(input, 'rb')}
+    # Send file
+    def send_file(self, input, datatype):
+        url = 'https://www.pdfexaminer.com/pdfapi.php'
+        files = {'sample[]': open(input, 'rb')}
 
-    # Post request
-    r = requests.post(url, files=files, data=datatype)
-
-    print(r.text.strip())
+        # Post request
+        r = requests.post(url, files=files, data=datatype)
+        self.set_report(r)
 
 
+
+# Main
 def main():
-    args = parse_arguments()
+    pdfx = PdfexaminerSendfile()
+    args = pdfx.parse_arguments()
 
     # Use JSON format if not defined
     if not args.format:
@@ -47,19 +65,19 @@ def main():
         print("Unidentified format")
         sys.exit()
 
-    t1 = threading.Thread(target=send_file, args=(args.input,datatype,))
-    t2 = threading.Thread(target=progress_bar, args=("run",))
+    t1 = threading.Thread(target=pdfx.send_file, args=(args.input,datatype,))
+    t2 = threading.Thread(target=pdfx.progress_bar, args=("run",))
     t1.start()
     t2.start()
     t1.join()
 
+    # Print out report
+    print(pdfx.get_report())
+
     # Stop progress bar when send_file finishes
     if not t1.isAlive():
         t2.run = False
-        t2.join()
 
 
 if __name__ == '__main__':
     main()
-
-
