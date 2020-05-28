42d6413d46fba1d6fd2406b8b21dd7e1c38276a9
Ville Kalliokoski <vkalliokoski@gmail.com>
2020-05-28 13:23:49 +0300

    Added test for running with a sample file

--- pdfxray-lite/test_pdfxray_lite.py
+++ pdfxray-lite/test_pdfxray_lite.py
@@ -1,9 +1,17 @@
 from metatool import dockertools
 import pytest
 
-@pytest.mark.dev
+#@pytest.mark.dev
+
+SAMPLE_FILE="_samples/pdf/general_test_file.pdf"
+
 
 def test_help():
     tool = dockertools.tool_with_file(__file__)
     out = tool.run_get_string([])
     assert out.startswith("Usage: pdfxray_lite.py [options]")
+
+def test_with_pdf():
+    tool = dockertools.tool_with_file(__file__)
+    out = tool.run_get_string(["-f", SAMPLE_FILE, "-r", "."])
+    assert "report.html" in out.splitlines()[-1]
