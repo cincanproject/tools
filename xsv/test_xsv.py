e2e23b4d6e6a282342893671919e3abe4001a399
Kimmo Linnavuo <kimmo.linnavuo@ficora.fi>
2020-06-03 12:58:31 +0300

    Add tests, fix Dockerfile

--- xsv/test_xsv.py
+++ xsv/test_xsv.py
@@ -1,9 +1,13 @@
 from metatool import dockertools
 import pytest
 
+def test_headers(tool):
+    out = tool.run_get_string(["headers", "_samples/txt/test.csv"])
+    assert "1   foo" in out
+    assert "2   bar" in out
 
 def test_count(tool):
-    out = tool.run_get_string(["frequency", "_samples/test.csv"])
+    out = tool.run_get_string(["frequency", "_samples/txt/test.csv"])
     assert "foo,42,1" in out
 
 @pytest.fixture(scope='function')
