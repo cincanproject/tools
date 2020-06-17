c6c1f8f41daa2e94277db0be29e822efb6b88d5e
Rloota <juhakalkainen@gmail.com>
2020-06-17 12:35:55 +0300

    snowman-decompile update

--- snowman-decompile/test_snowman_decompile.py
+++ snowman-decompile/test_snowman_decompile.py
@@ -1,9 +1,7 @@
 from metatool import dockertools
 import pytest
 
-@pytest.mark.dev
-
 def test_help():
     tool = dockertools.tool_with_file(__file__)
     out = tool.run_get_string([])
-    assert out.startswith("FIXME")
+    assert out.startswith("Usage: nocode [options] [--] file...")
