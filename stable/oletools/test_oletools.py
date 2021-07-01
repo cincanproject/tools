70d25ba8e7148020d67ee2353496e1762da3fa73
Niklas Saari <niklas.saari@tutanota.com>
2021-07-01 16:55:50 +0300

    oletools: update to 0.60

--- stable/oletools/test_oletools.py
+++ stable/oletools/test_oletools.py
@@ -5,6 +5,43 @@ SAMPLE_FILE="samples/msoffice/very_suspicious.doc"
 
 # NOTE: No tests for all tools yet. 
 
+OLEID_OUT = """
+Filename: samples/msoffice/very_suspicious.doc
+--------------------+--------------------+----------+--------------------------
+Indicator           |Value               |Risk      |Description               
+--------------------+--------------------+----------+--------------------------
+File format         |MS Word 97-2003     |info      |                          
+                    |Document or Template|          |                          
+--------------------+--------------------+----------+--------------------------
+Container format    |OLE                 |info      |Container type            
+--------------------+--------------------+----------+--------------------------
+Application name    |Microsoft Office    |info      |Application name declared 
+                    |Word                |          |in properties             
+--------------------+--------------------+----------+--------------------------
+Properties code page|1252: ANSI Latin 1; |info      |Code page used for        
+                    |Western European    |          |properties                
+                    |(Windows)           |          |                          
+--------------------+--------------------+----------+--------------------------
+Author              |Mallory             |info      |Author declared in        
+                    |                    |          |properties                
+--------------------+--------------------+----------+--------------------------
+Encrypted           |False               |none      |The file is not encrypted 
+--------------------+--------------------+----------+--------------------------
+VBA Macros          |Yes                 |Medium    |This file contains VBA    
+                    |                    |          |macros. No suspicious     
+                    |                    |          |keyword was found. Use    
+                    |                    |          |olevba and mraptor for    
+                    |                    |          |more info.                
+--------------------+--------------------+----------+--------------------------
+XLM Macros          |No                  |none      |This file does not contain
+                    |                    |          |Excel 4/XLM macros.       
+--------------------+--------------------+----------+--------------------------
+External            |0                   |none      |External relationships    
+Relationships       |                    |          |such as remote templates, 
+                    |                    |          |remote OLE objects, etc   
+--------------------+--------------------+----------+--------------------------
+"""
+
 def test_entrypoint():
     tool = dockertools.tool_with_file(__file__)
     out = tool.run_get_string([])
@@ -23,7 +60,7 @@ def test_olevba():
 def test_oleid():
     tool = dockertools.tool_with_file(__file__)
     out = tool.run_get_string(["oleid", SAMPLE_FILE])
-    assert out.endswith("\n Flash objects                  0                        \n\n")
+    assert out.endswith(OLEID_OUT)
 
 def test_oledir():
     tool = dockertools.tool_with_file(__file__)
