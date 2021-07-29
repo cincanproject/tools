941266da093152a440bbe1853c444fdd352bcb74
Niklas Saari <niklas.saari@tutanota.com>
2021-07-29 19:18:47 +0300

    radare2: update to 5.3.1, fix ghidra plugin and add test for it

--- stable/radare2/test_radare2.py
+++ stable/radare2/test_radare2.py
@@ -81,13 +81,47 @@ def test_rax2():
     assert out.startswith("Usage: rax2 [options] [expr ...]")
 
 
+pdf_out = """            ; DATA XREF from entry0 @ 0x1061
+/ 35: main ();
+|           0x00001139      55             push rbp
+|           0x0000113a      4889e5         mov rbp, rsp
+|           0x0000113d      488d3dc40e00.  lea rdi, str.Hello__World_  ; 0x2008 ; "Hello, World!"
+|           0x00001144      e8e7feffff     call sym.imp.puts
+|           0x00001149      488d3dc80e00.  lea rdi, str.And_Hello_for_radare2_as_well_ ; 0x2018 ; "And Hello for radare2 as well!"
+|           0x00001150      e8dbfeffff     call sym.imp.puts
+|           0x00001155      b800000000     mov eax, 0
+|           0x0000115a      5d             pop rbp
+\           0x0000115b      c3             ret
+WARNING: No calling convention defined for this file, analysis may be inaccurate.
+Warning: set your favourite calling convention in `e anal.cc=?`
+\x1b[2K\r"""
+
 def test_simple_inline_analysis():
     tool = dockertools.tool_with_file(__file__)
-    out = tool.run_get_string(["r2", "-e", "bin.cache=true", "-Aqc", "'pdf @main'", SAMPLE_FILE])
-    assert out.startswith("\x1b[2K\r")
+    out = tool.run_get_string(["r2", "-e", "bin.cache=true", "-e", "scr.color=false", "-Aqc", "pdf @main", SAMPLE_FILE])
+    print(out)
+    assert out.endswith(pdf_out)
 
 
 def test_grapscript_no_args_error():
     tool = dockertools.tool_with_file(__file__)
     out = tool.run_get_string(["script", "r2_callgraph.sh"])
     assert out.startswith("No sample directory provided. ")
+
+ghidra_out = """
+undefined8 main(void)
+
+{
+    sym.imp.puts("Hello, World!");
+    sym.imp.puts("And Hello for radare2 as well!");
+    return 0;
+}
+WARNING: No calling convention defined for this file, analysis may be inaccurate.
+Warning: set your favourite calling convention in `e anal.cc=?`
+\x1b[2K\r"""
+
+def test_ghidra_plugin():
+    tool = dockertools.tool_with_file(__file__)
+    out = tool.run_get_string(["r2", "-e", "bin.cache=true", "-e", "scr.color=false", "-Aqc", "pdg @main", SAMPLE_FILE])
+    assert out.endswith(ghidra_out)
+
