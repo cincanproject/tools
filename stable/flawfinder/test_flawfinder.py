bae7c4f37aa2f7fd0f4aae6de4f743e33ea6841a
Niklas Saari <niklas.saari@tutanota.com>
2021-07-30 19:47:08 +0300

    flawfinder: update to 2.0.17, add CMD and entrypoint test

--- stable/flawfinder/test_flawfinder.py
+++ stable/flawfinder/test_flawfinder.py
@@ -2,10 +2,31 @@ from testing import dockertools
 
 SAMPLE_FILE="samples/source/c/overflow.c"
 
-def test_help():
+
+default_opts = """
+flawfinder [--help | -h] [--version] [--listrules]
+  [--allowlink] [--followdotdir] [--nolink]
+           [--patch filename | -P filename]
+  [--inputs | -I] [--minlevel X | -m X]
+           [--falsepositive | -F] [--neverignore | -n]
+  [--context | -c] [--columns | -C] [--dataonly | -D]
+           [--html | -H] [--immediate | -i] [--singleline | -S]
+           [--omittime] [--quiet | -Q]
+  [--loadhitlist F] [--savehitlist F] [--diffhitlist F]
+  [--] [source code file or source root directory]+
+
+  The options cover various aspects of flawfinder as follows.
+"""
+
+def test_entrypoint():
     tool = dockertools.tool_with_file(__file__)
     out = tool.run_get_string([])
-    assert out.startswith("Flawfinder version")
+    assert out.startswith(default_opts)
+
+def test_help():
+    tool = dockertools.tool_with_file(__file__)
+    out = tool.run_get_string(["--help"])
+    assert out.startswith(default_opts)
 
 def test_analysis():
     tool = dockertools.tool_with_file(__file__)
