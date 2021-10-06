3b17ef97595092165a3cd51103cc7c3803de3759
Niklas Saari <niklas.saari@tutanota.com>
2021-10-07 02:03:03 +0300

    jd-cli: update to 1.2.1 and build from source from now on

--- stable/jd-cli/test_jd-cli.py
+++ stable/jd-cli/test_jd-cli.py
@@ -6,7 +6,7 @@ SAMPLE_FILE="samples/java/HexDump.class"
 def test_help():
     tool = dockertools.tool_with_file(__file__)
     out = tool.run_get_string([])
-    assert out.startswith("jd-cli version 1.2.0 - Copyrigh")
+    assert out.startswith("jd-cli version 1.2.1 - Copyrigh")
 
 def test_decompile():
     tool = dockertools.tool_with_file(__file__)
